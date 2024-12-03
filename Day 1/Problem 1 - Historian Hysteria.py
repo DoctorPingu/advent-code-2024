01331312357570d057ba0177f640b11b46120b07
Doctor Pingu <mohkod717@gmail.com>
2024-12-03 16:45:57 +1100

    Updated Files

---
+++ Day 1/Problem 1 - Historian Hysteria.py
@@ -0,0 +1,23 @@
+file_path = "E:\Visual Studio Code\Python\Advent of Code - 2024\Day 1/Puzzle Input.txt"
+
+def distance_calculator(file_path):
+    left_side = []
+    right_side = []
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            left, right = map(int, line.split())
+            left_side.append(left)
+            right_side.append(right)
+        
+        left_side.sort()
+        right_side.sort()
+
+    total_distance = 0
+    for i in range(len(left_side)):
+        total_distance += abs(left_side[i] - right_side[i])
+
+    return total_distance
+
+
+print(distance_calculator(file_path))
---
+++ Day 1/Problem 2 - Historian Hysteria.py
@@ -0,0 +1,27 @@
+file_path = "E:\Visual Studio Code\Python\Advent of Code - 2024\Day 1/Puzzle Input.txt"
+
+def similarity(file_path):
+    left_side = []
+    right_side = []
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            left, right = map(int, line.split())
+            left_side.append(left)
+            right_side.append(right)
+        
+        left_side.sort()
+        right_side.sort()
+
+    right_count = {}
+    for num in right_side:
+        right_count[num] = right_count.get(num, 0) + 1
+
+    similarity_score = 0
+    for num in left_side:
+        if num in right_side:
+            similarity_score += num * right_count[num]
+
+    return similarity_score
+
+print(similarity(file_path))
